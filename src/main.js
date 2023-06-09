5c21587d107dd2fd968960397b8f2d7bc47e7895
阿储 <108052715+star9710@users.noreply.github.com>
2023-06-09 17:33:12 +0800

    引入接口

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import axios from 'axios'
 
 createApp(App).mount('#app')
+/* 挂载全局对象 */
+app.config.globalProperties.$axios = axios;
